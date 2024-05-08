09d8b0da58decb1eb61783b2ed6e0e19da295425
Ivan Kot <ivaniko4738@hu@gmail.com>
2024-05-08 17:25:32 +0300

    first release

--- configs/config.py
+++ configs/config.py
@@ -1,9 +1,9 @@
 
-IAM_TOKEN = "t1.9euelZrIzJmamJCPiZuenoyOjpbPyu3rnpWamZ6RnZGVyJfPnZbIxs7IjMnl8_cvIRJO-e8NRUNL_d3z929PD0757w1FQ0v9zef1656VmpHOx5bMnZSayJiXmcyLkZCe7_zF656VmpHOx5bMnZSayJiXmcyLkZCeveuelZqXzsbMmZydk8bMi46QncyKzLXehpzRnJCSj4qLmtGLmdKckJKPioua0pKai56bnoue0oye.-RiZe8UMM54c0RvZxnRqyL1cj0NNa8-aQxGCUJoWvbbzur_AB5a8lV0Y5YhosVHemCMYRIl8LGM_pS5wycazBQ"
+IAM_TOKEN = ""
 
-FOLDER_ID = "b1ggl5ujmq7hn5b0to43"
+FOLDER_ID = ""
 
-TOKEN = "6671793616:AAEwJwYW5dg6hEpBTPXsdWrajg3yZJ6sJHc"
+TOKEN = ""
 
 
 ABOUT = "Я умный голосовой помощник. Проси все что хочешь. Я понимаю только аудио и текст."
